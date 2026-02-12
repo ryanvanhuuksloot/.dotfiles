6c20162f20c91a791b5fdca26eedc1d482d18085
Ryan van Huuksloot <ryan.vanhuuksloot@shopify.com>
2026-02-12 14:30:48 -0500

    Clean up new_computer.sh to use Brewfile and install.sh

--- new_computer.sh
+++ new_computer.sh
@@ -1,19 +1,12 @@
+#!/bin/bash
+set -e
+
 # Install Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+eval "$(/opt/homebrew/bin/brew shellenv)"
 
-# Install Homebrew packages
-brew install kubectx
-brew install fzf
-brew install ripgrep
+# Install all packages from Brewfile
+brew bundle --file="$HOME/.dotfiles/brew/Brewfile"
 
-# IDE Configurations
-# ===========================
-# Configs in ~/.configs/ghostty && ~/.configs/zellij && ~/.configs/starship.toml
-# Install Starship
-curl -fsSL https://starship.rs/install.sh | bash -s -- --yes
-# Install Zellij
-brew install zellij
-# Install Ghostty (GUI for Zellij)
-brew install ghostty@tip
-# Font is JetBrains Mono
-brew install --cask jetbrains-mono
+# Set up symlinks for configs
+"$HOME/.dotfiles/install.sh"
